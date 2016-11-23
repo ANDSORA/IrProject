0a6f383b9efe0b917c3748d63aa5d921be9ed90a
ANDSORA <xuyin@student.ethz.ch>
2016-11-23 12:46:26 +0100

    test for breeze and jar in lib

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,7 +1,15 @@
 /**
   * Created by andsora on 11/16/16.
   */
+import breeze.linalg.DenseVector
+import ch.ethz.dal.tinyir.processing.ReutersRCVParse
+import ch.ethz.dal.tinyir.processing.StopWords
+
 object hello extends App {
+  val a = DenseVector.fill(100, 0.1)
+  val b = List("are", "we", "world")
+  println(a(2))
+  println(StopWords.filter(b))
   println("Hello, IrProject.")
   println("Yes, and here is Li")
 }
